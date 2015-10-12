db9204d4bb8c586ea9fee5c71614bdea00932c44
Vasiliy Schitov <alibabaih@gmail.com>
2015-10-12 18:09:35 +0300

    Work with 2-1 task

---
+++ 1-3/test/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+ *    Functions, Objects
+ *    See WEB Training I-3 Practice.
+ *    Implement the following Underscore.js functions: 
+ *    bind, bindAll, partial, memoize, delay, defer, 
+ *    throttle, debounce, once, after, before, wrap, negate, compose.
+ *    See http://underscorejs.org/#functions for more details and examples.
+ *    tests
+*/
+
+function bind() {
+    
+}
