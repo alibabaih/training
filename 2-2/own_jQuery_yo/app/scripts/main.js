478ef88325448884339ff0a77d9e3b86a0e557dd
alibabaih <alibabaih@gmail.com>
2015-10-19 18:01:42 +0300

    jQuery YO

---
+++ 2-2/own_jQuery_yo/app/scripts/main.js
@@ -0,0 +1,8 @@
+sung('.change-width').width(200);
+sung('.change-height').height(70);
+sung('.find-class').hasClass('cool-class');
+sung('.add-class').addClass('added-class');
+sung('.nice-class').removeClass('scram');
+sung('.add-material').append(' <strong>Appended</strong>').prepend('<strong>Prepended</strong> ');
+sung('.add-material').wrap('<div class="wrapped"></div>');
+sung('.toggle').toggleClass('toggleable');
